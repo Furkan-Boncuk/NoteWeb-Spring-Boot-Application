66385f19f0feac5e8ac8fed25d16695deb82649c
Furkan Boncuk <furkanboncuk2000@gmail.com>
2023-06-20 15:42:34 +0300

    Entity Class

--- src/main/java/com/furkanboncuk/NoteWeb/entity/Note.java
+++ src/main/java/com/furkanboncuk/NoteWeb/entity/Note.java
@@ -1,4 +1,40 @@
 package com.furkanboncuk.NoteWeb.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="notes_id")
+    private long id;
+    @Column(name="notes_title",nullable = false)
+    @NotBlank(message="The 'title' field must not be left blank")
+    private String title;
+    @Column(name="notes_category",nullable = false)
+    @NotBlank(message="The 'category' field must not be left blank")
+    private String category;
+    @CreationTimestamp
+    @Column(name="notes_created_date",nullable = false)
+    private Date createdDate;
+    @UpdateTimestamp
+    @Column(name="notes_updated_date", nullable = false)
+    private Date updatedDate;
+    @Column(name="note_content", nullable = false)
+    @NotBlank(message="The 'content' field must not be left blank")
+    private String content;
 }
