620abf839c9243acb6e496780c15231f31a9c888
Furkan Boncuk <furkanboncuk2000@gmail.com>
2023-06-22 16:24:46 +0300

    NoteRestController.java

--- src/main/java/com/furkanboncuk/NoteWeb/controller/NoteRestController.java
+++ src/main/java/com/furkanboncuk/NoteWeb/controller/NoteRestController.java
@@ -2,23 +2,17 @@ package com.furkanboncuk.NoteWeb.controller;
 
 import com.furkanboncuk.NoteWeb.entity.Note;
 import com.furkanboncuk.NoteWeb.exception.NoteDoesNotExistException;
-import com.furkanboncuk.NoteWeb.exception.NoteInformationException;
 import com.furkanboncuk.NoteWeb.exception.NoteTitleException;
-import com.furkanboncuk.NoteWeb.repository.NoteRepository;
 import com.furkanboncuk.NoteWeb.service.NoteServiceImpl;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @RestController
@@ -36,13 +30,13 @@ public class NoteRestController {
     public ResponseEntity<Object> createNewNote(@Valid @RequestBody Note note) throws NoteTitleException {
         List<Note> existingNotes = noteService.findAllNotes();
 
-        for(Note obj : existingNotes) {
-            if(obj.getTitle().equals(note.getTitle())) {
+        for (Note obj : existingNotes) {
+            if (obj.getTitle().equals(note.getTitle())) {
                 throw new NoteTitleException();
             }
         }
         noteService.saveNote(note);
-        return new ResponseEntity<>(note,HttpStatus.OK);
+        return new ResponseEntity<>(note, HttpStatus.OK);
     }
 
     // localhost:4000/notes/all
@@ -55,7 +49,7 @@ public class NoteRestController {
     // localhost:4000/notes/update/{id}
     @PutMapping("/update/byId/{id}")
     public ResponseEntity<Note> updateNoteById(@Valid @PathVariable("id") Long id,
-                                                @RequestBody Note note) {
+                                               @RequestBody Note note) {
         Note newNote = new Note();
         Optional<Note> oldNote = noteService.getNoteById(id);
 
@@ -63,20 +57,20 @@ public class NoteRestController {
         newNote.setTitle(note.getTitle());
         newNote.setContent(note.getContent());
         newNote.setCategory(note.getCategory());
-        newNote.setUpdatedDate(Date.from(Instant.now()));
+        newNote.setUpdatedDate(LocalDateTime.now());
 
-        if(newNote.getTitle().equals(oldNote.get().getTitle()) && newNote.getContent().equals(oldNote.get().getContent())) {
+        if (newNote.getTitle().equals(oldNote.get().getTitle()) && newNote.getContent().equals(oldNote.get().getContent())) {
             throw new NoteTitleException();
         } else {
             noteService.saveNote(newNote);
-            return new ResponseEntity<>(newNote,HttpStatus.OK);
+            return new ResponseEntity<>(newNote, HttpStatus.OK);
         }
     }
 
     // localhost:4000/notes/update/{title}
     @PutMapping("/update/byTitle/{title}")
     public ResponseEntity<Note> updateNoteByTitle(@Valid @PathVariable("title") String title,
-                                               @RequestBody Note note) {
+                                                  @RequestBody Note note) {
         Note newNote = new Note();
         Optional<Note> oldNote = noteService.getNoteByTitle(title);
 
@@ -84,55 +78,33 @@ public class NoteRestController {
         newNote.setTitle(note.getTitle());
         newNote.setContent(note.getContent());
         newNote.setCategory(note.getCategory());
-        newNote.setUpdatedDate(Date.from(Instant.now()));
+        newNote.setUpdatedDate(LocalDateTime.now());
 
-        if(newNote.getTitle().equals(oldNote.get().getTitle()) && newNote.getContent().equals(oldNote.get().getContent())) {
+        if (newNote.getTitle().equals(oldNote.get().getTitle()) && newNote.getContent().equals(oldNote.get().getContent())) {
             throw new NoteTitleException();
         } else {
             noteService.saveNote(newNote);
-            return new ResponseEntity<>(newNote,HttpStatus.OK);
+            return new ResponseEntity<>(newNote, HttpStatus.OK);
         }
     }
-    /*
-    @PutMapping({"/update/{keyword}"})
-    public ResponseEntity<Note> updateNoteById(@Valid @PathVariable("keyword") String keyword,
-                             @RequestBody Note note) {
-        Note newNote = new Note();
-        Note oldNote;
-        if(keyword.matches("\\d+")) {
-            Long id = Long.parseLong(keyword);
-            oldNote = noteService.getNoteById(id);
-        } else {
-            oldNote = noteService.getNoteByTitle(keyword);
-        }
-
-        if(oldNote!=null) {
-            newNote.setId(oldNote.getId());
-            newNote.setCreatedDate(oldNote.getCreatedDate());
-            newNote.setTitle(note.getTitle());
-            newNote.setContent(note.getContent());
-            newNote.setCategory(note.getCategory());
-            newNote.setUpdatedDate(Date.from(Instant.now()));
-
-            noteService.saveNote(newNote);
-            return new ResponseEntity<>(newNote,HttpStatus.CREATED);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }*/
 
     //localhost:4000/notes/delete/byId/{id}
     @DeleteMapping("/delete/byId/{id}")
     public String deleteNoteByIdMethod(@PathVariable("id") Long id) {
-        noteService.deleteNoteById(id);
-        return "Note was deleted, successfully : "+id;
+        Optional<Note> note = noteService.getNoteById(id);
+        if (note.isPresent()) {
+            noteService.deleteNoteById(id);
+            return "Note was deleted, successfully : " + id;
+        } else {
+            throw new NoteDoesNotExistException();
+        }
     }
 
     //localhost:4000/notes/delete/byTitle/{title}
     @DeleteMapping("/delete/byTitle/{title}")
     public String deleteNoteByTitleMethod(@PathVariable String title) {
         Optional<Note> note = noteService.getNoteByTitle(title);
-        if(note.isPresent()) {
+        if (note.isPresent()) {
             noteService.deleteNoteByTitle(title);
             return "Note was deleted successfully: " + title;
         } else {
@@ -140,16 +112,103 @@ public class NoteRestController {
         }
     }
 
-    /*
-    @DeleteMapping("/delete/{keyword}")
-    public String deleteNote(@PathVariable("keyword") String keyword) {
-        if(keyword.matches("\\d+")) {
-            Long id = Long.parseLong(keyword);
-            noteService.deleteNoteById(id);
+    //localhost:4000/searchNote/byId/{id}
+    @GetMapping("/searchNote/byId/{id}")
+    public ResponseEntity<List<Note>> displayNoteById(@PathVariable("id") Long id) {
+        List<Note> notes = noteService.findAllNotes().stream().filter(item -> item.getId()==id).collect(Collectors.toList());
+        if (notes.isEmpty()) {
+            throw new NoteDoesNotExistException();
         } else {
-            noteService.deleteNoteByTitle(keyword);
+            return new ResponseEntity<>(notes,HttpStatus.OK);
         }
-        return "Note was deleted, successfully";
     }
-*/
+
+    //localhost:4000/searchNote/byTitle/{title}
+    @GetMapping("/searchNote/byTitle/{title}")
+    public ResponseEntity<List<Note>> displayNoteByTitle(@PathVariable("title") String title) {
+        List<Note> notes = noteService.findAllNotes().stream().filter(item -> item.getTitle().equals(title)).collect(Collectors.toList());
+        if (notes.isEmpty()) {
+            throw new NoteDoesNotExistException();
+        } else {
+            return new ResponseEntity<>(notes,HttpStatus.OK);
+        }
+    }
+
+    //localhost:4000/searchNote/byCategory/{category}
+    @GetMapping("/searchNote/byCategory/{category}")
+    public ResponseEntity<List<Note>> displayNotesByCategory(@PathVariable("category") String category) {
+        List<Note> notes = noteService.findAllNotes().stream().filter(item -> item.getCategory().equals(category)).collect(Collectors.toList());
+        if (notes.isEmpty()) {
+            throw new NoteDoesNotExistException();
+        } else {
+            return new ResponseEntity<>(notes,HttpStatus.OK);
+        }
+    }
+
+    //localhost:4000/filterNote/byTitle/{title}
+    @GetMapping("/filterNote/byTitle")
+    public ResponseEntity<?> filterNotesByTitleParam(@RequestParam(required = false) String title) {
+        if(title==null || title.isEmpty()) {
+            return ResponseEntity.badRequest().body("Title parameter is required.");
+        }
+
+        try {
+            List<Note> filteredNotes = noteService.filterNoteByTitle(title);
+            if (filteredNotes.isEmpty()) {
+                return ResponseEntity.notFound().build();
+                //throw new NoteDoesNotExistException();
+            }
+            return ResponseEntity.ok(filteredNotes);
+
+        } catch (NoteDoesNotExistException ex) {
+            return ResponseEntity.notFound().build();
+            //throw new NoteDoesNotExistException();
+        }
+    }
+
+    //localhost:4000/filterNote/byCategory/{category}
+    @GetMapping("/filterNote/byCategory")
+    public ResponseEntity<?> filterNotesByCategoryParam(@RequestParam(required = false) String category) {
+        if (category == null || category.isEmpty()) {
+            return ResponseEntity.badRequest().body("Category parameter is required.");
+        }
+
+        try {
+            List<Note> filteredNotes = noteService.filterNoteByCategory(category);
+            if (filteredNotes.isEmpty()) {
+                return ResponseEntity.notFound().build();
+                //throw new NoteDoesNotExistException();
+            }
+            return ResponseEntity.ok(filteredNotes);
+
+        } catch (NoteDoesNotExistException ex) {
+            return ResponseEntity.notFound().build();
+            //throw new NoteDoesNotExistException();
+        }
+    }
+
+    /*
+    //localhost:4000/filterNote/byUpdatedDate/{updatedDate}
+    @GetMapping("/filterNote/byUpdatedDate")
+    public ResponseEntity<?> filterNotesByUpdatedDateParam(@RequestParam(required = false) LocalDateTime updatedDate) {
+        String formattedDate = updatedDate.toString();
+        if (formattedDate.equals(null)) {
+            return ResponseEntity.badRequest().body("Updated Date parameter is required.");
+        }
+
+        try {
+            List<Note> filteredNotes = noteService.filterNoteByUpdatedDate(formattedDate);
+            if (filteredNotes.isEmpty()) {
+                return ResponseEntity.notFound().build();
+                //throw new NoteDoesNotExistException();
+            }
+            return ResponseEntity.ok(filteredNotes);
+
+        } catch (NoteDoesNotExistException ex) {
+            return ResponseEntity.notFound().build();
+            //throw new NoteDoesNotExistException();
+        }
+    }
+    */
+
 }
